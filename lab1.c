511cbb8e7e805e55025003a262b5fad19b20f05f
Vovan <manta@localhost.localdomain>
2019-09-09 21:17:18 +0300

    Почти конец

--- lab1.c
+++ lab1.c
@@ -4,23 +4,30 @@
 int main()
 {
     char firm1[9], firm2[9], firm3[9];
-    int kolvprod1, kolvprod2, kolvprod3;
+    int kolvprod1, kolvprod2, kolvprod3, leng;
     unsigned int yearprod1, yearprod2, yearprod3;
     float rinok1, rinok2, rinok3;
     printf("Введите название фирмы, количество продуктов, годовой объем продажи($) и часть рынка(%) \n");
-    scanf("%s %d",firm1, &kolvprod1);
+    scanf("%s %d %u %f",firm1, &kolvprod1, &yearprod1, &rinok1);
 
-    printf(firm1);
-    printf("\n");
+    printf("Введите название фирмы, количество продуктов, годовой объем продажи($) и часть рынка(%) \n");
+    scanf("%s %d %u %f",firm2, &kolvprod2, &yearprod2, &rinok2);
+
+    printf("Введите название фирмы, количество продуктов, годовой объем продажи($) и часть рынка(%) \n");
+    scanf("%s %d %u %f",firm3, &kolvprod3, &yearprod3, &rinok3);
 
 
 
-    printf("------------------------------------------------------------------------\n");
+
+
+    printf("|------------------------------------------------------------------------|\n");
     printf("|Фирмы - производители СКБД\t\t\t\t\t\t        |\n");
-    printf("|Фирмы\t|Количество. продуктов\t|Годовой объем продажи ($)\t|Часть рынка (%)|\n");
-    printf("|Фирма1\t|\t\t|\t\t|\t\t\t\t|\n");
-    printf("|Фирма2\t|\t\t|\t\t|\t\t\t\t|\n");
-    printf("|Фирма3\t|\t\t|\t\t|\t\t\t\t|\n");
+    printf("|Фирмы|Количество. продуктов|Годовой объем продажи ($)|Часть рынка (%)   |\n");
+    printf("|%s|%d|%u|%f|\n", firm1, kolvprod1, yearprod1, rinok1);
+    printf("|Фирма2||||\n");
+    printf("|%s|%d|%u|%f|\n", firm2, kolvprod2, yearprod2, rinok2);
+    printf("|Фирма3||||\n");
+    printf("|%s|%d|%u|%f|\n", firm3, kolvprod3, yearprod3, rinok3);
     printf("|Примечание: по данным Gartner Group за 1999г\t\t\t\t|\n");
         printf("------------------------------------------------------------------------\n");
 
