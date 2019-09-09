5d25aa37560e96086f8d8b93850ac13e429bde0b
Vovan <manta@localhost.localdomain>
2019-09-09 20:23:58 +0300

    Передел

--- lab1.c
+++ lab1.c
@@ -4,9 +4,13 @@
 int main()
 {
     char firm1[9], firm2[9], firm3[9];
+    int kolvprod1, kolvprod2, kolvprod3;
+    unsigned int yearprod1, yearprod2, yearprod3;
+    float rinok1, rinok2, rinok3;
     printf("Введите название фирмы, количество продуктов, годовой объем продажи($) и часть рынка(%) \n");
-    scanf("%s",firm1);
-    printf(firm);
+    scanf("%s %d",firm1, &kolvprod1);
+
+    printf(firm1);
     printf("\n");
 
 
